e59cea5e645b58788756f47e33cd2ff29b5d3181
Six <84689659+SixofClubsss@users.noreply.github.com>
2023-12-24 09:45:07 +0000

    0.1.1 tag

--- dimport/dimport.go
+++ dimport/dimport.go
@@ -22,7 +22,7 @@ import (
 
 var logger = structures.Logger.WithFields(logrus.Fields{})
 
-var version = semver.MustParse("0.1.0-dev")
+var version = semver.MustParse("0.1.1")
 
 // Get current package version
 func Version() semver.Version {
